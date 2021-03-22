5331d39352934183881ee0bf3ec37d1b95742a30
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-03-22 15:46:44 -0400

    commit 530655799 on 20210322

--- ma.bash
+++ ma.bash
@@ -11,6 +11,7 @@ cd "$JDR"
 _AT_ bvalosek/cpuspy 6221a583f95a18b6563c178368fe8cd99fde3949
 # _AT_ bvalosek/cpuspy 8ca2080cd4f54c4dc9626be4cbdb16273049ea91
 _AT_ dmarszk/sdkmanager 6355c02251ce4a9776724eb5e7b51e8a7a08d54d
+_AT_ mikedg1/fullscreen-checker d56f3ff95a888a11d730c1e9ec5bcc332685ad1a
 _AT_ rkkr/simple-keyboard 439a5e64bede9c65dae6ebafc9b8a67b6d353cb3
 _AT_ ss/android-demo f39147045b71480f5f8d5e86208d7ccd8bf10550
 # ma.bash OEF
