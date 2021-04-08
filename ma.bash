2d3ff23ea761560bb2954114bb80f55a4a2d626f
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-04-08 11:29:15 -0400

    commit 225493505 on 20210408

--- ma.bash
+++ ma.bash
@@ -10,7 +10,11 @@ cd "$JDR"
 # _AT_ username/repository commit
 _AT_ bvalosek/cpuspy 6221a583f95a18b6563c178368fe8cd99fde3949
 _AT_ dmarszk/sdkmanager 6355c02251ce4a9776724eb5e7b51e8a7a08d54d
+_AT_ ldilley/chmodcalc 70bb2e8051f83dd8e56f02566f7b55a918e85bc5
+_AT_ ldilley/umaskcalc 83e6e74f64db9f5f9cbf695e4cf72b5a2b48321b
+_AT_ ldilley/unixtime 31abb10a3bb9f3709b0cd7d15ed46d3a2fe86c99
 _AT_ mikedg1/fullscreen-checker d56f3ff95a888a11d730c1e9ec5bcc332685ad1a
 _AT_ shakalaca/USB-OTG-Manager 3f08acb5d5aadfa1e38ec78138633ac55241281a
 _AT_ ss/android-demo f39147045b71480f5f8d5e86208d7ccd8bf10550
+_AT_ thomasheller/android-notes 615b1c27cbfd66c9a0c683e2e8e430855e06bf34
 # ma.bash OEF
