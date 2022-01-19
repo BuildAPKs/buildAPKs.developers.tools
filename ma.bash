7c89ec8b1b0dbfb9efab5dbdb876a8e546ead996
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-01-18 23:50:49 -0500

    commit 474826283 on 20220118

--- ma.bash
+++ ma.bash
@@ -10,6 +10,7 @@ cd "$JDR"
 # _AT_ username/repository commit
 _AT_ Fuzion24/Permission-Control 8257b96ca05db6d19dad4cb6103cd5d211e7ccd0
 _AT_ bvalosek/cpuspy 6221a583f95a18b6563c178368fe8cd99fde3949
+_AT_ denghui1010/android_shell 91c068b69924a871238c2227d47946082dff4470
 _AT_ dmarszk/sdkmanager 6355c02251ce4a9776724eb5e7b51e8a7a08d54d
 _AT_ jack-carter/DeviceExplorer 0e81ed378adc87af95e60978088e92109ace9dbf
 _AT_ jack-carter/ThemeExplorer 874c61529288f6852467f6e1e8c9904e0213fcf6
@@ -23,6 +24,6 @@ _AT_ ss/android-demo f39147045b71480f5f8d5e86208d7ccd8bf10550
 _AT_ thomasheller/android-notes 615b1c27cbfd66c9a0c683e2e8e430855e06bf34
 _AT_ wenpincui/SensorData 91a371ddb4e775b78254e8d4485998b83a1d45de
 _AT_ yigiter/DataMonitor 2c3958228e9b1ed45359b13a7bcbf0e354727d6b
-_AT_ yussidivnall/Android-Terminal 5ea9b50bd9b5f85f5b2787cf402fe58635b89ae9
+_AT_ yussidivnall/Android-Terminal bfab6d8ad342022e0ed4920dc6f94adb8c373ac5
 _AT_ yussidivnall/Android-test-1 aba770d546792c50f6ffc173e5dd7ad3a82ddb51
 # ma.bash OEF
